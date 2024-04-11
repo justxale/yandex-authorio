5cc4f04416ce41e2022ae154210a680186c40aa8
Xale <alexev2401@gmail.com>
2024-04-11 19:28:18 +0300

    remove unused field

--- server.py
+++ server.py
@@ -43,8 +43,7 @@ def register():
                                    message="Такой пользователь уже есть")
         user = User(
             name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
+            email=form.email.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
