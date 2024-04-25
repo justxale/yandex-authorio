358abec53e47f57c52e3c455edf05fcb35ee4a90
Xale <alexev2401@gmail.com>
2024-04-25 22:34:32 +0300

    pool fix and profile style edit

--- data/db_session.py
+++ data/db_session.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy import NullPool
 from sqlalchemy.orm import Session
 
 SqlAlchemyBase = orm.declarative_base()
@@ -19,7 +20,7 @@ def global_init(db_file):
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False, poolclass=NullPool)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
